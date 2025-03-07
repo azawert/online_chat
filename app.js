23b16d0ebf31d0c8ac0f5427420c997817d8f9a0
azawert <53710085+azawert@users.noreply.github.com>
2025-03-07 14:46:25 +0300

    feat: added swagger doc

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import userRoutes from './src/routes/user.js'
 import http from 'http'
 import { Server } from 'socket.io'
 import path from 'path';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { setupSocket } from './src/controllers/index.js';
+import express from 'express'
 
 const app = createApp()
 
@@ -18,10 +21,44 @@ const __dirname = path.resolve();
 
 setupSocket(io)
 
+const swaggerOptions = {
+	swaggerDefinition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Online chat api',
+			version: '1.0.0',
+			description: 'API documentation',
+		},
+		servers: [
+			{
+				url: `http://localhost:4001`,
+			},
+			{
+				url: `ws://localhost:4001`,
+				description: 'WebSocket server',
+			},
+		],
+		components: {
+			securitySchemes: {
+				bearerAuth: {
+					type: 'http',
+					scheme: 'bearer',
+					bearerFormat: 'JWT',
+				},
+			},
+		},
+	},
+	apis: ["./src/routes/*.js", "./src/docs/websocket.yaml"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/api/user', userRoutes)
 app.get('/', (_, res) => {
 	res.sendFile(path.join(__dirname, 'public', '/index.html'))
 })
+
 
 
 server.listen(port, () => console.log(`Server is active on port ${port}`));
